c3c55b1556fb2215feaf1e4ba5772bc3fff0f3c2
Quentin <quentin.consigny@isen.yncrea.fr>
2019-11-07 11:02:16 +0100

    Fonction remplissage dictionnaire
    INSERTools Possède maintenant InsertDICTIONNARY. Prends en entrée une liste de ['function','shortdesc','longdesc'] et un ID. Remplis avec la table DICTIONNARY.

--- InsertTools.py
+++ InsertTools.py
@@ -25,7 +25,7 @@ def InsertID(askedname):
 def InsertCOMMANDS(destination,function,source,com=None):
     try:
         connection = mysql.connector.connect(host='localhost',database='heronDatabase',
-        user='root',password='HeronLeR0B0T')
+        user='robot',password='HeronLeR0B0T')
         if(com!=None):
             mySql_insert_query = "INSERT INTO COMMANDS (OrderId, Function,Target,Status,Source,ComOrder) VALUES ('ToDo','"+function+"','"+destination+"','waiting','"+source+"','"+com+"');"
         else:
@@ -42,3 +42,19 @@ def InsertCOMMANDS(destination,function,source,com=None):
         print("Failed to insert record into AVAILABLE {}".format(error))
 
     return True
+
+def InsertDICTIONNARY(FunctionList,ID):
+    for elt in FunctionList:
+            function,shortDescription,longDescription=elt[0],elt[1],elt[2]
+            connection = mysql.connector.connect(host='localhost',database='heronDatabase',
+            user='robot',password='HeronLeR0B0T')
+            mySql_insert_query = "INSERT INTO DICTIONNARY (ID,Function, ShortDescription,LongDescription) VALUES ('"+ID+"','"+function+"','"+shortDescription+"','"+longDescription+"');"
+            cursor = connection.cursor()
+            result = cursor.execute(mySql_insert_query)
+            connection.commit()
+            cursor.close()
+
+listFunction=[["function 1","shortDescription1","longDesciption1"],["function 2","shortDescription2","longDesciption2"]]
+ID="centraltest-01"
+
+InsertDICTIONNARY(listFunction,ID)
