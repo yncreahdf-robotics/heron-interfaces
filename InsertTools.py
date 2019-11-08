06f83f9ddeca558da1d33a17b443f700d2b3b698
Quentin <quentin.consigny@isen.yncrea.fr>
2019-11-08 14:16:50 +0100

    Tools use by SQLTools for INSERT function

--- InsertTools.py
+++ InsertTools.py
@@ -4,29 +4,29 @@ import time
 
 
 def FindID(name):
-    i=1
-    while(CountID("ID='"+name+"-"+str(i)+"'")):
-        i+=1
-    return name+"-"+str(i)
+    print(CountID(name))
+    return (name+"-"+str(CountID(name)))
 
-def CountID(condition):
+def CountID(name):#retourne le premier indice libre d'un nom. i.e. si heron-1 et heron-2 sont déjà utilisé CountID("heron") retourne 3
 
-    try:
-        connection = mysql.connector.connect(host='localhost',database='heronDatabase',
-        user='robot',password='HeronLeR0B0T')
+    connection = mysql.connector.connect(host='localhost',database='heronDatabase',
+    user='robot',password='HeronLeR0B0T')
+    sql_select_Query = "select * from AVAILABLE;"
+    cursor = connection.cursor()
+    cursor.execute(sql_select_Query)
+    records = cursor.fetchall()
+    Test=True
+    it=0
+    while(Test):
+        Test=False
+        for elt in records:
+            if(elt[1]==name+"-"+str(it)):
+                Test=True
+                it+=1
 
-        sql_select_Query = "select * from AVAILABLE where "+condition+";"
-        cursor = connection.cursor()
-        cursor.execute(sql_select_Query)
-        records = cursor.fetchall()
-        #{print("la longueur : ",len(records))
-        for row in records:
-            #print(row)
-            pass
-    except :
-        print("Error reading data from MySQL table")
+    return(it)
 
-def InsertID(askedname):
+def InsertID(askedname):#Utilise CountID pour trouver l'indice à utiliser. Insert dans AVAILABLE le nom demandé + indice et retourne nom+"-"+ID à utiliser
     askedname=FindID(askedname)
     try:
         connection = mysql.connector.connect(host='localhost',database='heronDatabase',
@@ -46,14 +46,14 @@ def InsertID(askedname):
 
     return askedname
 
-def InsertCOMMANDS(destination,function,source,com=None):
+def InsertCOMMANDS(ref,destination,function,source,com=None):#Insert dans COMMANDS (OrderID,Target,Function,Source,ComOrder). ComOrder est facultatif, la commands serat automatiquement en wainting
     try:
         connection = mysql.connector.connect(host='localhost',database='heronDatabase',
         user='robot',password='HeronLeR0B0T')
         if(com!=None):
-            mySql_insert_query = "INSERT INTO COMMANDS (OrderId, Function,Target,Status,Source,ComOrder) VALUES ('ToDo','"+function+"','"+destination+"','waiting','"+source+"','"+com+"');"
+            mySql_insert_query = "INSERT INTO COMMANDS (OrderId, Function,Target,Status,Source,ComOrder) VALUES ('"+ref+"','"+function+"','"+destination+"','waiting','"+source+"','"+com+"');"
         else:
-            mySql_insert_query = "INSERT INTO COMMANDS (OrderId, Function,Target,Status,Source) VALUES ('ToDo','"+function+"','"+destination+"','waiting','"+source+"');"
+            mySql_insert_query = "INSERT INTO COMMANDS (OrderId, Function,Target,Status,Source) VALUES ('"+ref+"','"+function+"','"+destination+"','waiting','"+source+"');"
 
         #print("sql insert query")
         #print(mySql_insert_query)
@@ -67,7 +67,7 @@ def InsertCOMMANDS(destination,function,source,com=None):
 
     return True
 
-def InsertDICTIONNARY(FunctionList,ID):
+def InsertDICTIONNARY(FunctionList,ID):#Liste de function [Function,ShortDesc,LongDesc]
     for elt in FunctionList:
             function,shortDescription,longDescription=elt[0],elt[1],elt[2]
             connection = mysql.connector.connect(host='localhost',database='heronDatabase',
@@ -78,7 +78,7 @@ def InsertDICTIONNARY(FunctionList,ID):
             connection.commit()
             cursor.close()
 
-def ChangeCOMMANDS(status,line):
+def ChangeCOMMANDS(status,line):#Change le status de la ligne de clé primaire line
 
     line=str(line)
     connection = mysql.connector.connect(host='localhost',database='heronDatabase',user='root',password='HeronLeR0B0T')
@@ -88,13 +88,13 @@ def ChangeCOMMANDS(status,line):
     sql_select_Query += line
     sql_select_Query +='"'
 
-    print(sql_select_Query)
+    #print(sql_select_Query)
     cmd="""mysql -u 'robot' --password='HeronLeR0B0T' -e '"""
     cmd+=sql_select_Query
     cmd+="'"
-    print("cmd:",cmd)
+    #print("cmd:",cmd)
     os.system(cmd)
-    print(sql_select_Query)
+    #print(sql_select_Query)
     cursor = connection.cursor()
     #connection.commit()
     cursor.execute(sql_select_Query)
