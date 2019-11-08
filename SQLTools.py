fa1b01a67b5c96d98c151961f86f52c0273095bd
Quentin <quentin.consigny@isen.yncrea.fr>
2019-11-08 14:17:11 +0100

    Automatic SQL querry

--- SQLTools.py
+++ SQLTools.py
@@ -7,13 +7,13 @@ import time
 from DisplayTools import *
 from InsertTools import *
 
-def Clean(table):
+def Clean(table):#Réinitialise une table
 
     try:
         connection = mysql.connector.connect(host='localhost',database='heronDatabase',
-        user='robot',password='HeronLeR0B0T')
+        user='robot',password='HeronLeR0B0T')#Connection
 
-        mySql_insert_query = "TRUNCATE TABLE "+table+";"
+        mySql_insert_query = "TRUNCATE TABLE "+table+";"#Command SQL Nescessaire pour réinitialiser une table
 
         cursor = connection.cursor()
         result = cursor.execute(mySql_insert_query)
@@ -23,10 +23,9 @@ def Clean(table):
     except mysql.connector.Error as error:
         print("Delet Fail")
 
-
-
     finally:
         if (connection.is_connected()):
             connection.close()
-            cursor.close()
+            cursor.close()#Fermeture de la connexion
+            
     return len(records)
