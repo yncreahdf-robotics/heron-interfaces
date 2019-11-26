c90487c638a305c65f63954958906b0ea22d9af8
Quentin <quentin.consigny@isen.yncrea.fr>
2019-11-26 11:38:11 +0100

    Ready for a test on Jetson

--- InitConnectionClient.py
+++ InitConnectionClient.py
@@ -5,9 +5,12 @@
 """
 
 import socket
+import SQLTools
+import SELECTTools
+import InsertTools
+import time
 
-
-
+ListFunction=[['DICTIONNARY','DICTIONNARY',''],['PRINT','PRINT(str)','PRINT ON DEVICE'],['TEMPERATURE','TEMPERATURE(nZone)','Return in Comm the TEMPERATURE of the zone : BCPU-therm / MCPU-therm / GPU-therm / PLL-therm / Tboard_tegra / Tdiode_tegra / PMIC-Die / thermal-fan-est']]
 
 def RecovPass():#Récupère la phrase de passe à l'intérieur du fichier et stock la chaîne de caractère dans une variable (retournée)
     f= open("../Guess.txt","r")
@@ -34,4 +37,29 @@ def InitConnection(name):
     s.close()#Fermeture de la connection
     return data2
 
-#print(InitConnection("TestNewProgram"))
+ID=InitConnection("NewDevice")
+
+print("----------------------")
+
+InsertTools.InsertCOMMANDS('fortheTest',ID,'TEMPERATURE(4)','fortheTest',com=None)
+
+now=time.time()
+while(time.time()-now<10):
+    #print(time.time()-now)
+    commands=SELECTTools.CommandsFor(ID)
+    for elt in commands:
+        LineOrder,OrderID,Function,Target,Status,Source,ComOrder=elt[0],elt[1],elt[2],elt[3],elt[4],elt[5],elt[6]
+        if(Status=='waiting'):
+                if(Status=='waiting'):
+
+                    if(Function=='DICTIONNARY'):
+                        print("DICTIONNARY")
+                        InsertTools.ChangeCOMMANDS("accepted",LineOrder)
+                        InsertTools.InsertDICTIONNARY(ListFunction,ID)
+                        InsertTools.ChangeCOMMANDS("done",LineOrder)
+
+                    if('TEMPERATURE' in Function):
+                        print("TEMPERATURE")
+                        InsertTools.ChangeCOMMANDS("accepted",LineOrder)
+                        InsertTools.UpdateCom('Temp'+str(int(Function[-2])),LineOrder)
+                        InsertTools.ChangeCOMMANDS("done",LineOrder)
