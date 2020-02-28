af68c88747cd1cce354403f1be60d44dfe6b060d
Sylvain Louvet <sylvain.louvet@isen.yncrea.fr>
2020-02-28 18:06:46 +0100

    Code commenté pour les menus

--- heron_menu/heron_menu.py
+++ heron_menu/heron_menu.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 import roslaunch
 import rospy
@@ -8,17 +9,19 @@ import subprocess
 import os
 import time
 
+#Mode du menu
 menu = ['Remote Control', 'Navigation', 'Mapping', 'Take Key Positions', 'IP Config (Restart after)', 'Exit']
 
+#Fonction pour modifier les IP aux lignes ROS_MASTER_URI et ROS_HOSTNAME du .zshrc
 def modifIP(stdscr):
     stdscr.clear()
-    curses.echo()
+    curses.echo()  #Affiche les caractères que l'on tape sur le clavier
     stdscr.addstr("Adresse IP PC Central ? (Enter to continue, NO SPACES)\nExample : 10.224.0.52\n")
-    ip_master = stdscr.getstr()
+    ip_master = stdscr.getstr() #Récupère l'IP master
     stdscr.clear()
     stdscr.addstr("Adresse IP Heron ? (Enter to continue, NO SPACES)\nExample : 10.224.0.51\n")
-    ip_heron = stdscr.getstr()
-    curses.noecho()
+    ip_heron = stdscr.getstr() #Récupère l'IP heron
+    curses.noecho() #Désactive le fait d'afficher les caractères
 
     file = open("/home/nvidia/.zshrc", "r")
     content=file.readlines()
@@ -35,6 +38,7 @@ def modifIP(stdscr):
     file2.close()
 
 
+#Permet d'afficher le menu en récupérant la hauteur de l'écran et la largeur pour écrire les textes de manière centrée
 def print_menu(stdscr, selected_row_idx):
     stdscr.clear()
     h, w = stdscr.getmaxyx()
@@ -44,7 +48,7 @@ def print_menu(stdscr, selected_row_idx):
     for idx, row in enumerate(menu):
         x = w//2 - len(row)//2
         y = h//2 - len(menu)//2 + idx
-        if idx == selected_row_idx:
+        if idx == selected_row_idx: #Met en blanc le fond du mode sur lequel on se trouve
             stdscr.attron(curses.color_pair(1))
             stdscr.addstr(y, x, row)
             stdscr.attroff(curses.color_pair(1))
@@ -52,13 +56,16 @@ def print_menu(stdscr, selected_row_idx):
             stdscr.addstr(y, x, row)
     stdscr.refresh()
 
+#Change les variables d'environnement de ROS_MASTER_URI et ROS_HOSTNAME
 def set_environment(master, ip):
     os.environ['ROS_MASTER_URI']=master
     os.environ['ROS_HOSTNAME']=ip
 
+#Récupère les variables d'environnement de ROS_MASTER_URI et ROS_HOSTNAME 
 def get_environment():
     return os.environ['ROS_MASTER_URI'], os.environ['ROS_HOSTNAME']
 
+#Affiche les textes de manière centrée 
 def print_center(stdscr, text):
     stdscr.clear()
     h, w = stdscr.getmaxyx()
@@ -67,16 +74,17 @@ def print_center(stdscr, text):
     stdscr.addstr(y, x, text)
     stdscr.refresh()
 
+#Permet de lancer un launch
 def start_launch(launch_name, stdscr):
     stdscr.nodelay(1)
     gpio.setup(396,gpio.IN)
     uuid = roslaunch.rlutil.get_or_generate_uuid(None, True)
     roslaunch.configure_logging(uuid)
     launch = roslaunch.parent.ROSLaunchParent(uuid, ['/home/nvidia/catkin_ws/src/heron_software/src/launch/'+launch_name+'.launch'])
-    launch.start()
-    while stdscr.getch() != 27 and gpio.read(396) != 0:
+    launch.start() #Lance le launch
+    while stdscr.getch() != 27 and gpio.read(396) != 0: #Attend que "Echap" ou le bouton d'arrêt d'urgence soit appuyé
         pass
-    launch.shutdown()
+    launch.shutdown() #Arrête le launch
 
 def main(stdscr):
     # turn off cursor blinking
@@ -94,39 +102,38 @@ def main(stdscr):
     while 1:
         key = stdscr.getch()
 
-        if key == curses.KEY_UP and current_row > 0:
+        if key == curses.KEY_UP and current_row > 0: #Permet de monter dans les modes
             current_row -= 1
-        elif key == curses.KEY_DOWN and current_row < len(menu)-1:
+        elif key == curses.KEY_DOWN and current_row < len(menu)-1: #Permet de descendre dans les modes
             current_row += 1
-        elif key == curses.KEY_ENTER or key in [10, 13]:
-            #print_center(stdscr, "You selected '{}'".format(menu[current_row]))
+        elif key == curses.KEY_ENTER or key in [10, 13]: #Permet de sélectionner un mode
             stdscr.clear()
             stdscr.refresh()
-            if current_row == 0:
-                master, ip = get_environment()
-                set_environment("http://localhost:11311","localhost")
-                roscore = subprocess.Popen('roscore')
+            if current_row == 0: #Mode manette
+                master, ip = get_environment() #Récupère les IP actuelles du master et du robot
+                set_environment("http://localhost:11311","localhost") #Définit le master et le hostname en local
+                roscore = subprocess.Popen('roscore') #Lance un roscore
                 time.sleep(1)
-                start_launch('heronController', stdscr)
+                start_launch('heronController', stdscr) #Lance le launch manette
                 time.sleep(1)
-                roscore.terminate()
+                roscore.terminate() #Quitte le roscore
                 time.sleep(1)
-                set_environment(master,ip)
-            if current_row == 1:
+                set_environment(master,ip) #Remet les anciennes IP du master et du hostname
+            if current_row == 1: #Mode Navigation
                 start_launch('bringUp', stdscr)
-            if current_row == 2:
+            if current_row == 2: #Mode mapping
                start_launch('gmapping', stdscr)
-            if current_row == 3:
+            if current_row == 3: #Mode prise de positions
                 start_launch('take_pos', stdscr)
             # if user selected last row, exit the program
-            if current_row == 4:
+            if current_row == 4: #Mode config IP
                 stdscr.addstr("Are you sure ? Press ENTER and it will delete the old IP\nRestart the menu when finished to save the changes")
                 key = stdscr.getch()
-                if key == curses.KEY_ENTER or key in [10, 13]:
+                if key == curses.KEY_ENTER or key in [10, 13]: #Si appui sur entrée
                     modifIP(stdscr)
-                else:
-                    pass
-            if current_row == len(menu)-1:
+                else: #Si appui sur autre touche
+                    pass #Revient au menu
+            if current_row == len(menu)-1: #Ferme le menu
                 break
 
         print_menu(stdscr, current_row)
--- heron_menu/master_menu.py
+++ heron_menu/master_menu.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 import roslaunch
 import rospy
@@ -8,18 +9,20 @@ import os
 import time
 import sys
 
+#Mode du menu
 menu = ['Navigation', 'Mapping', 'Take Key Positions', 'Edit Key Positions', 'IP Config (Restart after)', 'Exit']
 
-
+#Fonction pour modifier les IP aux lignes ROS_MASTER_URI et ROS_HOSTNAME du .zshrc
+#Modifie aussi le fichier HeronMasterStartup.desktop pour lancer le menu en ssh sur le robot 
 def modifIP(stdscr):
     stdscr.clear()
-    curses.echo()
+    curses.echo()  #Affiche les caractères que l'on tape sur le clavier
     stdscr.addstr("Adresse IP PC Central ? (Enter to continue, NO SPACES)\nExample : 10.224.0.52\n")
-    ip_master = stdscr.getstr()
+    ip_master = stdscr.getstr() #Récupère l'IP master
     stdscr.clear()
     stdscr.addstr("Adresse IP Heron ? (Enter to continue, NO SPACES)\nExample : 10.224.0.51\n")
-    ip_heron = stdscr.getstr()
-    curses.noecho()
+    ip_heron = stdscr.getstr() #Récupère l'IP heron
+    curses.noecho() #Désactive le fait d'afficher les caractères
 
     file = open("/home/centralheron/.zshrc", "r")
     content=file.readlines()
@@ -32,7 +35,7 @@ def modifIP(stdscr):
 
     file2 = open("/home/centralheron/.zshrc", "w")
     for line in content:
-        file2.write(line)
+        file2.write(line)   #Ecrit la totalité du fichier .zshrc avec les IP changées
     file2.close()
 
     file3 = open("/home/centralheron/Desktop/HeronMasterStartup.desktop", "r")
@@ -44,13 +47,13 @@ def modifIP(stdscr):
     file4 = open("/home/centralheron/Desktop/HeronMasterStartup.desktop", "w")
 
     for line in content:
-        file4.write(line)
+        file4.write(line)  #Ecrit la totalité du fichier HeronMasterStartup.desktop en changeant l'IP en ssh
 
     file4.close()
 
 
 
-
+#Permet d'afficher le menu en récupérant la hauteur de l'écran et la largeur pour écrire les textes de manière centrée
 def print_menu(stdscr, selected_row_idx):
     stdscr.clear()
     h, w = stdscr.getmaxyx()
@@ -60,7 +63,7 @@ def print_menu(stdscr, selected_row_idx):
     for idx, row in enumerate(menu):
         x = w//2 - len(row)//2
         y = h//2 - len(menu)//2 + idx
-        if idx == selected_row_idx:
+        if idx == selected_row_idx: #Met en blanc le fond du mode sur lequel on se trouve
             stdscr.attron(curses.color_pair(1))
             stdscr.addstr(y, x, row)
             stdscr.attroff(curses.color_pair(1))
@@ -68,6 +71,7 @@ def print_menu(stdscr, selected_row_idx):
             stdscr.addstr(y, x, row)
     stdscr.refresh()
 
+#Affiche les textes de manière centrée 
 def print_center(stdscr, text):
     stdscr.clear()
     h, w = stdscr.getmaxyx()
@@ -76,31 +80,33 @@ def print_center(stdscr, text):
     stdscr.addstr(y, x, text)
     stdscr.refresh()
 
+#Permet de lancer un roscore en parallèle, de lancer un launch également
 def start_launch(launch_name, stdscr, current_row):
-    roscore = subprocess.Popen('roscore')
+    roscore = subprocess.Popen('roscore') #Roscore lancé en threading
     time.sleep(1)
-    stdscr.nodelay(1)
+    stdscr.nodelay(1) 
     uuid = roslaunch.rlutil.get_or_generate_uuid(None, True)
     roslaunch.configure_logging(uuid)
     launch = roslaunch.parent.ROSLaunchParent(uuid, ['/home/centralheron/catkin_ws/src/heron_software/src/launch/'+launch_name+'.launch'])
-    launch.start()
-    while stdscr.getch() != 27:
+    launch.start() #Lance le launch
+    while stdscr.getch() != 27: #Attend que "Echap" soit appuyé 
         pass
-    launch.shutdown()
-    if current_row == 1:
+    launch.shutdown() #Arrête le launch
+    if current_row == 1: #Si le mode est "mapping"
         rosrun = "rosrun map_server map_saver map:=/"+os.getenv("HERON_ID")+"/map -f /home/centralheron/catkin_ws/src/heron_software/map/demo"
-        subprocess.check_call(rosrun.split())
-    roscore.terminate()
+        subprocess.check_call(rosrun.split()) #Lance la commande pour sauvegarder la map
+    roscore.terminate() #Arrête le roscore
     time.sleep(1)
 
-
+#Permet de lancer le programme pour éditer les positions 
 def start_program1(stdscr):
     command = "python3 /home/centralheron/heron-interfaces/interface_graphique_modif_pos/modifPositions.py"
     program = subprocess.Popen(command.split())
     print_center(stdscr, "Click on [close] to get back to the menu.")
-    while program.poll() == None:
+    while program.poll() == None: #Attend que le programme se termine
         pass
 
+#Permet de lancer le programme pour envoyer des positions au robot
 def start_program2():
     command = "python3 /home/centralheron/heron-interfaces/navigation/navigationGoal.py"
     return subprocess.Popen(command.split())
@@ -122,47 +128,45 @@ def main(stdscr):
     while 1:
         key = stdscr.getch()
         stdscr.nodelay(0)
-        if key == curses.KEY_UP and current_row > 0:
+        if key == curses.KEY_UP and current_row > 0: #Permet de monter dans les modes
             current_row -= 1
-        elif key == curses.KEY_DOWN and current_row < len(menu)-1:
+        elif key == curses.KEY_DOWN and current_row < len(menu)-1: #Permet de descendre dans les modes
             current_row += 1
-        elif key == curses.KEY_ENTER or key in [10, 13]:
-            #print_center(stdscr, "You selected '{}'".format(menu[current_row]))
+        elif key == curses.KEY_ENTER or key in [10, 13]: #Permet de sélectionner un mode
             stdscr.clear()
             stdscr.refresh()
-            if current_row == 0:
-                prog = start_program2()
-                start_launch('master_navigation', stdscr,current_row)
-                prog.terminate()
-            if current_row == 1:
+            if current_row == 0: #Mode Navigation
+                prog = start_program2() #Lance le programme d'envoi de positions
+                start_launch('master_navigation', stdscr,current_row) #Lance le launch
+                prog.terminate() #Coupe le programme d'envoi de positions
+            if current_row == 1: #Mode Mapping
                 stdscr.addstr("Are you sure ? \nPress ENTER and it will delete the old map.\nPress any other key to go back to menu.")
                 key = stdscr.getch()
-                if key == curses.KEY_ENTER or key in [10, 13]:
+                if key == curses.KEY_ENTER or key in [10, 13]: #Appui Entrée
                     stdscr.clear()
                     start_launch('display_mapping', stdscr,current_row)
-                else:
-                    pass
-            if current_row == 2:
+                else: #Si appui sur une autre touche
+                    pass #Revient au menu
+            if current_row == 2: #Mode Prise de positions
                 stdscr.addstr("Are you sure ? \nPress ENTER and it will delete the old positions.\nPress any other key to go back to menu.")
                 key = stdscr.getch()
-                if key == curses.KEY_ENTER or key in [10, 13]:
+                if key == curses.KEY_ENTER or key in [10, 13]: #Si appui sur entrée
                     stdscr.clear()
                     start_launch('take_key_position',stdscr,current_row)
-                else:
-                    pass
+                else: #Si appui sur une autre touche
+                    pass #Revient au menu
                 
-            if current_row == 3:
-                start_program1(stdscr)
+            if current_row == 3: #Mode Edition de positions
+                start_program1(stdscr) #Lance le programme pour éditer les positions
 
-            if current_row == 4:
+            if current_row == 4: #Mode config IP
                 stdscr.addstr("Are you sure ? Press ENTER and it will delete the old IP\nRestart the menu when finished to save the changes")
                 key = stdscr.getch()
-                if key == curses.KEY_ENTER or key in [10, 13]:
+                if key == curses.KEY_ENTER or key in [10, 13]: #Si appui sur entrée
                     modifIP(stdscr)
-                else:
-                    pass
-            # if user selected last row, exit the program
-            if current_row == len(menu)-1:
+                else: #Si appui sur une autre touche
+                    pass #Revient au menu
+            if current_row == len(menu)-1: #Ferme le menu
                 break
 
         print_menu(stdscr, current_row)
